d1cda5bee6d1dbcaa8352e0e96452e50c2a80f04
Aditya Kumar <neelaadityakumar@gmail.com>
2025-02-22 17:38:51 +0530

    add

--- react-app/src/component/comment/index.jsx
+++ react-app/src/component/comment/index.jsx
@@ -11,16 +11,16 @@ const CommentBox = ({ addComment }) => {
   };
 
   return (
-    <div className="comment-details p-2 border rounded mt-2">
+    <div className="p-2 border rounded mt-2">
       <input
         type="text"
         placeholder="Add text here"
-        className="input p-2 border rounded w-full"
+        className="p-2 border rounded w-full"
         value={text}
         onChange={(e) => setText(e.target.value)}
       />
       <button
-        className="btn submit bg-blue-500 text-white py-1 px-3 rounded mt-2"
+        className="bg-blue-500 text-white py-1 px-3 rounded mt-2"
         onClick={handleSubmit}
       >
         Submit
@@ -29,7 +29,7 @@ const CommentBox = ({ addComment }) => {
   );
 };
 
-const Comment = ({ text, onReply }) => {
+const Comment = ({ text }) => {
   const [showInputBox, setShowInputBox] = useState(false);
   const [replies, setReplies] = useState([]);
 
@@ -39,11 +39,11 @@ const Comment = ({ text, onReply }) => {
   };
 
   return (
-    <div className="all-comment p-2 border rounded mt-2">
+    <div className="p-2 border rounded mt-2">
       <div className="card p-2  rounded">
         <span className="text block">{text}</span>
         <span
-          className="reply text-blue-500 cursor-pointer mt-1 inline-block"
+          className="text-blue-500 cursor-pointer mt-1 inline-block"
           onClick={() => setShowInputBox(!showInputBox)}
         >
           Add Reply
@@ -52,7 +52,7 @@ const Comment = ({ text, onReply }) => {
 
       {showInputBox && <CommentBox addComment={addReply} />}
 
-      <div className="replies pl-4 border-l mt-2">
+      <div className="pl-4 border-l mt-2">
         {replies.map((reply, index) => (
           <Comment key={index} text={reply} onReply={addReply} />
         ))}
@@ -69,10 +69,10 @@ const CommentContainer = () => {
   };
 
   return (
-    <div className="comment-container p-4">
+    <div className="p-4">
       <CommentBox addComment={addComment} />
 
-      <div className="comments mt-4">
+      <div className="mt-4">
         {comments.map((comment, index) => (
           <Comment key={index} text={comment} />
         ))}
